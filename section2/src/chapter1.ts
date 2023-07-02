de79aa9d373a5e1400227b18b9ef224bf84c651b
kijiwon <kiji1313@gmail.com>
2023-07-02 23:44:22 +0900

    number타입

--- section2/src/chapter1.ts
+++ section2/src/chapter1.ts
@@ -1,2 +1,13 @@
 // number
-let num1:number = 123;
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+num1 = 'hello'; 
+num1.toUpperCase();
+num1.toExponential(); 
