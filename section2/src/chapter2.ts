48e3558015d4b7f75fd44e633647ef2c384142e2
kijiwon <kiji1313@gmail.com>
2023-07-03 14:12:21 +0900

    배열 타입

---
+++ section2/src/chapter2.ts
@@ -0,0 +1,7 @@
+// 배열
+let numArr:number[] = [1,2,3];
+let strArr:string[] = ['hello','welcome'];
+let boolArr: Array<boolean> = [true, false, true];
+
+// 배열의 요소들의 타입이 다양할 경우
+let multiArr = [1, 'hello'];
