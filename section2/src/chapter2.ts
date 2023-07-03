1b097d742b336f724a3f4982230e8f9f4a6df262
kijiwon <kiji1313@gmail.com>
2023-07-03 14:31:12 +0900

    다차원배열 타입

--- section2/src/chapter2.ts
+++ section2/src/chapter2.ts
@@ -5,3 +5,14 @@ let boolArr: Array<boolean> = [true, false, true];
 
 // 배열의 요소들의 타입이 다양할 경우
 let multiArr = [1, 'hello'];
+
+// 다차원 배열의 타입 정의하기
+let doubleArr:number[][] = [
+    [1,2,3],
+    [4,5]
+];
+
+let doubleStr:string[][]= [
+    ['hello', 'welcome'],
+    ['im', 'princess']
+]
