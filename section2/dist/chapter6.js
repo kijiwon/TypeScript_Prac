0b531bdcdaa3524d0289ea2f12579c0a95b59492
kijiwon <kiji1313@gmail.com>
2023-08-04 15:15:18 +0900

    chapter6:unknown

--- section2/dist/chapter6.js
+++ section2/dist/chapter6.js
@@ -11,4 +11,18 @@ anyVar.toFixed();
 // 타입이 정의된 변수에 할당할 수 있음
 let num = 10;
 num = anyVar;
+// unknown type
+let unknownVar;
+unknownVar = "";
+unknownVar = 10;
+unknownVar = {};
+unknownVar = () => { };
+// 어떤 타입의 변수에 할당하거나 연산, 메소드를 사용하는 것은 불가능
+// num = unknownVar;
+// unknownVar+1;
+// unknownVar.toFixed();
+// typeof 연산자를 이용해 값을 활용
+if (typeof unknownVar === 'number') {
+    num = unknownVar;
+}
 export {};
--- section2/src/chapter6.ts
+++ section2/src/chapter6.ts
@@ -12,3 +12,21 @@ anyVar.toFixed();
 // 타입이 정의된 변수에 할당할 수 있음
 let num:number = 10;
 num = anyVar;
+
+// unknown type
+let unknownVar:unknown;
+
+unknownVar = "";
+unknownVar = 10;
+unknownVar = {};
+unknownVar = ()=>{};
+
+// 어떤 타입의 변수에 할당하거나 연산, 메소드를 사용하는 것은 불가능
+// num = unknownVar;
+// unknownVar+1;
+// unknownVar.toFixed();
+
+// typeof 연산자를 이용해 값을 활용
+if(typeof unknownVar === 'number'){
+    num = unknownVar;
+}
