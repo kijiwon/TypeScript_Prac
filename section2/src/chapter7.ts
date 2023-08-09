066e30f06b2b5b1d6ff9b2cb43bace26b938f0a8
kijiwon <kiji1313@gmail.com>
2023-08-09 13:58:10 +0900

    chapter7:never

--- section2/src/chapter7.ts
+++ section2/src/chapter7.ts
@@ -18,3 +18,27 @@ let a:void;
 a=undefined;
 // tsconfig.json의 strictNullChecks설정을 false로 하면 null을 담을 수 있음
 // a=null;
+
+// never
+// never -> 존재하지 않는
+// 불가능한 타입
+
+// 무한루프를 타는 함수
+function func3():never{
+    while(true){}
+}
+
+// 의도적으로 오류를 발생시키는 함수 -> 반환값 없음
+function func4():never{
+    throw new Error();
+}
+
+// 변수에 정의할 시 any를 포함한 어떠한 타입의 값도 담을 수 없다.
+let anyVar:any;
+let b:never;
+// b=1;
+// b="hello";
+// b={};
+// b=undefined;
+// b=anyVar;
+// b=null
