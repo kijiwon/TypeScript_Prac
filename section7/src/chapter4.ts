b4ebcc3b4f3d28b69df12e88e48f823021753b7b
kijiwon <kiji1313@gmail.com>
2023-09-14 17:13:56 +0900

    chapter4:제네릭 인터페이스

---
+++ section7/src/chapter4.ts
@@ -0,0 +1,48 @@
+/**
+ * 제네릭 인터페이스
+ */
+
+interface KeyPair<K, V>{
+    key: K;
+    value: V;
+};
+
+let keyPair: KeyPair<string, number> ={
+    key: 'key',
+    value: 0,
+};
+
+let keyPair2: KeyPair<boolean, string[]> = {
+    key: true,
+    value: ['1'],
+};
+
+
+/**
+ * 인덱스 시그니쳐와 함께 사용하기
+ */
+
+// 인덱스 시그니쳐 
+// -> 프로퍼티의 키와 밸류의 타입에 관련된 규칙만 만족하면 어떠한 객체든 허용하는 유연한 객체 타입을 만듬
+
+interface NumberMap {
+    [key: string]: number;
+};
+
+let numberMap: NumberMap = {
+    key: -123,
+    key2: 123123,
+};
+
+// 제네릭 인터페이스와 사용
+interface Map<V> {
+    [key: string]: V;
+};
+
+let booleanMap: Map<boolean> = {
+    key: true,
+};
+
+let stringMap: Map<string> = {
+    key: "value",
+  };
