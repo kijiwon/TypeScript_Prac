efcafd77dc4c59ab690fb817d7e781c7dae4de00
kijiwon <kiji1313@gmail.com>
2023-09-15 22:21:50 +0900

    chapter4:제네릭 클래스

--- section7/src/chapter4.ts
+++ section7/src/chapter4.ts
@@ -61,3 +61,55 @@ type Map2<V> = {
 let stringMap2: Map2<string> = {
     key: 'hallo',
 };
+
+
+
+
+/**
+ * 제네릭 인터페이스의 활용 예시
+ * 유저 관리 프로그램
+ * -> 유저 구분 : 학생 유저 / 개발자 유저
+ */
+
+// 두 인터페이스는 서로소 집합
+interface Student {
+    type: 'student';
+    school: string;
+};
+
+interface Developer {
+    type: 'developer';
+    skill: string;
+};
+
+// User 인터페이스를 제네릭 인터페이스로 바꾸기
+interface User<T> {
+    name: string;
+    profile: T;
+};
+
+// 학생 유저만 사용하는 함수
+function goToSchool(user: User<Student>){
+    if(user.profile.type !=='student'){
+        console.log('잘 못 오셨습니다');
+        return;
+    };
+    const school = user.profile.school;
+    console.log(`${school}로 등교 완료`);
+};
+
+const developerUser: User<Developer> = {
+    name: '기지',
+    profile: {
+       type: 'developer',
+       skill: 'TypeScript', 
+    },
+};
+
+const studentUser: User<Student> = {
+    name: '죠니',
+    profile: {
+        type: 'student',
+        school: '단국대학교',
+    },
+};
