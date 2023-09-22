fb289d1f787862ef86fd41daccf858eac4fc83c5
kijiwon <kiji1313@gmail.com>
2023-09-22 19:49:55 +0900

    chapter2:keyof연산자

---
+++ section8/src/chapter2.ts
@@ -0,0 +1,38 @@
+/**
+ * keyof 연산자
+ */
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+// 주의!
+// keyof 연산자는 타입에만 사용할 수 있음
+// -> keyof person ❌
+function getPropertyKey(person, key: keyof Person){
+    return person[key];
+}
+
+const person: Person = {
+    name: '기지',
+    age: 27,
+};
+
+getPropertyKey(person, 'name');
+
+
+// typeof연산자와 함께 사용 가능
+// 타입을 정의할 때 사용하면 어떤 변수의 타입을 뽑아오는 역할을 함
+type Person2 = typeof person2;
+
+// Person2에는 person2에서 뽑아온 객체 타입이 들어가 있음
+// => Person2 대신 typeof person2로 사용 가능
+function getPropertyKey2(person, key: keyof typeof person2){ // key에는 'name' | 'age' 유니온 타입이 들어감
+    return person[key];
+}
+
+const person2 = {
+    name: '기지',
+    age: 27,
+};
