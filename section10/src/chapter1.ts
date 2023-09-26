3f9bb6ad2cbe33c8cdcc692231854504c32eb0a2
kijiwon <kiji1313@gmail.com>
2023-09-26 21:32:00 +0900

    chapter1:Required

--- section10/src/chapter1.ts
+++ section10/src/chapter1.ts
@@ -23,3 +23,27 @@ const draft: Partial<Post> = {
     title: '제목 나중에 짓자',
     content: '초안...',
 }
+
+
+
+
+
+/**
+ * Required<T>
+ * 필수의, 필수적인
+ * => 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+ */
+
+// Required 직접 구현
+type Required<T> = {
+    // 모든 프로퍼티에서 '선택적'이라는 기능을 빼주기
+    [key in keyof T]-?: T[key];
+}
+
+const withThumbnailPost: Required<Post> = {
+    title: '한입 타스 후기',
+    tag: ['ts'],
+    content: '',
+    // 선택적 프로퍼티인 thumbnailURL도 필수 프로퍼티가 됨
+    thumbnailURL: 'https://...',
+}
