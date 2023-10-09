a32740d6bd5c53b7acd535dba6f18c7101a3daa3
kijiwon <kiji1313@gmail.com>
2023-10-09 13:44:02 +0900

    chapter3:Extract

--- section10/src/chapter3.ts
+++ section10/src/chapter3.ts
@@ -21,3 +21,30 @@ type Exclude<T, U> = T extends U? never : T;
 type A = Exclude<string | boolean, boolean>
 // 타입 A는 string| boolean에서 boolean 타입을 제거한 타입
 // => string 타입
+
+
+
+
+/**
+ * Extract<T, U>
+ * 추출하다, 뽑아내다
+ * -> T에서 U를 추출하는 타입
+ */
+
+// Extract 직접 구현하기
+type Extract<T, U> = T extends U? T : never;
+// 타입 Extract는 타입 변수 T, U를 받아 T가 U의 확장이라면 T, 아니면 never
+// 1단계
+// Extract<string, boolean> |
+// Extract<boolean, boolean>
+
+// 2단계
+// never |
+// boolean
+
+// 결과
+// boolean <- never는 공집합이므로 합집합의 결과는 string
+
+type B = Extract<string | boolean, boolean>;
+// 타입 B는 string | boolean 에서 boolean 타입을 뽑아낸 타입
+// -> boolean 타입
